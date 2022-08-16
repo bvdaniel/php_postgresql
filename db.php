8763504296035453747438a8180475bdd4f8475a
Daniel Esteban Beltrán Varas <70292984+bvdaniel@users.noreply.github.com>
2022-08-16 08:53:07 -0400

    Update db.php
    updated for test postgresql

--- db.php
+++ db.php
@@ -1,13 +1,21 @@
 <?php
+class CConexion {
 //Configuracion necesaria para acceder a la base de datos.
-function conn(){
-    $hostname = "localhost";
-    $usuariodb = "root";
-    $passworddb = "*******";
-    $dbname = "REGISTRO";
-
-        //Genera conexión con servidor
-        $conectar = mysqli_connect($hostname, $usuariodb, $passworddb, $dbname);
-        return $conectar;
+function ConexionBD(){
+    $hostname = "10.128.0.6";
+    $usuariodb = "postgres";
+    $passworddb = "123";
+    $dbname = "registro";
+    try{
+                //Genera conexión con servidor
+        $conn = new PDO("pgsql:host= $hostname; dbname=$dbname", $usuariodb, $passworddb);
+        echo "Se conectó correctamente a la base de datos";
+    }
+    catch(PDOException $exp){
+        echo ("No se pudo conectar, $exp");
+    }
+    return $conn;
 } 
+}
 ?>
+
